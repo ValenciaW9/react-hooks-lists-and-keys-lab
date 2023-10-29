5fa1013b2b0b1274f5cab3662581ae6e7a0a70b8
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-29 14:07:16 -0400

    react-hooks-lists-and-keys-lab
    react-hooks-lists-and-keys-lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,18 +7,13 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
-        <a> <h3> Project Item </h3></a>
-        <div>{name}</div> <div>{about}</div>
-         <span className="technology">{technologies[0]}</span>
-         <span className="technology">{technologies[1]}</span>
-         <span className="technology">{technologies[2]}</span>
+        <a href="#">Project Item</a>
+        {technologies.map((technology) => (
+          <span className="technology">{technology}</span>
+        ))}
       </div>
     </div>
   );
 }
-//The project-item component should render a <h3> for the name, a <p> for the about, and a <div> for the technologies
-
 
 export default ProjectItem;
-
-
